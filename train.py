c338b096b6729e1ea87c309fdef4b88d5820f205
Kelvin <kelvinkonoor@gmail.com>
2024-06-18 11:15:00 +0530

    model training and saving

--- train.py
+++ train.py
@@ -14,3 +14,19 @@ SAMPLE_SIZE = 50000
 RANDOM_WALK_LEN = 128
 EMBED_DIMS = 256
 MODEL_NAME = 'myw2v.model'
+
+cites_df = pd.read_csv(os.path.join(DATASET_LOC, "cora.cites"), sep='\t', header=None, names=["target", "source"])
+content_df = pd.read_csv(os.path.join(DATASET_LOC, "cora.content"), sep='\t', header=None)[[0, 1434]]
+content_df.rename({0: 'node', 1434: 'type'}, axis = 1, inplace = True)
+
+# generating networkx graph from edgelist
+graph = nx.from_pandas_edgelist(cites_df)
+
+# model training
+dw = DeepWalk(graph)
+print('[x] generating training samples ...')
+trainX = dw.generate_train_samples(SAMPLE_SIZE, RANDOM_WALK_LEN)
+print('[x] model training ...')
+model = dw.train(trainX, EMBED_DIMS)
+print('[x] saving trained model ...')
+model.save(MODEL_NAME)
