770dafba663cfddefb30cd10b52dfafa07d1e9ce
Kelvin <kelvinkonoor@gmail.com>
2024-06-18 10:00:00 +0530

    generating random walks & training

--- deepwalk.py
+++ deepwalk.py
@@ -36,3 +36,42 @@ class DeepWalk:
             walk_length = walk_length - 1
 
         return walk
+    
+    def generate_train_samples(self, num_samples: int, walk_length: int) -> List:
+        """
+        Generate training samples for the DeepWalk model.
+
+        Args:
+            num_samples (int): The number of training samples to generate.
+            walk_length (int): The length of each random walk.
+
+        Returns:
+            List: A list of training samples, where each sample is a list of nodes.
+        """
+        X = []
+        for sid in tqdm(range(num_samples)):
+            node = random.choice(self.nodes)
+            walk = self.random_walk(node, walk_length)
+            X.append(walk)
+        return X
+    
+    def train(self, X: List, embed_dim:int = 128, window:int = 5, min_count:int = 1, workers:int = 4) -> W2V:
+        """
+        Train the DeepWalk model using the generated training samples.
+
+        Args:
+            X (List): A list of training samples, where each sample is a list of nodes.
+            embed_dim (int): The dimensionality of the word embeddings (default is 128).
+            window (int): The maximum distance between the current and predicted word within a sentence (default is 5).
+            min_count (int): Ignores all words with a total frequency lower than this (default is 1).
+            workers (int): The number of CPU cores to use for training (default is 4).
+
+        Returns:
+            gensim.models.word2vec.Word2Vec: The trained Word2Vec model.
+        """
+        w2v = Word2Vec(sentences = X, 
+              vector_size = embed_dim, 
+              window = window, 
+              min_count = min_count, 
+              workers = workers)
+        return w2v
