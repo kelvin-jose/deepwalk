8875d95ddcd09c324c4c9c022465df1363a7cb31
Kelvin <kelvinkonoor@gmail.com>
2024-06-19 10:00:00 +0530

    adding PCA support

--- train.py
+++ train.py
@@ -30,3 +30,17 @@ print('[x] model training ...')
 model = dw.train(trainX, EMBED_DIMS)
 print('[x] saving trained model ...')
 model.save(MODEL_NAME)
+
+k2i = pd.DataFrame({'node': model.wv.key_to_index.keys(), 'vindex': model.wv.key_to_index.values()})
+meta_df = pd.merge(content_df, k2i, on = 'node').sort_values('vindex')
+
+# converting embeddings to 2 dims for plotting 
+pca = PCA(n_components=2)
+x = pca.fit_transform(model.wv.vectors)
+
+meta_df['pca_x'] = x[:, 0]
+meta_df['pca_y'] = x[:, 1]
+meta_df['type_int'] = LabelEncoder().fit_transform(meta_df['type'])
+
+plt.scatter(meta_df['pca_x'], meta_df['pca_y'], c=meta_df['type_int'])
+plt.show()
